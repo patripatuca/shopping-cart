30a242069d5c7426fe1c601d429edf856fbd2c34
afd <afd@alup5.academiapostal.es.local>
2020-01-24 20:19:02 +0100

    calculos de totales y pesos

--- index.js
+++ index.js
@@ -6,7 +6,25 @@
  * @param {array de objetos} carrito 
  */
 export function calcularTotal(carrito) {
-  return 0;
+    var total=0;
+        for (var i=0 ; i<carrito.length ;i++){
+    if(carrito[i].cantidad){
+      total+=carrito[i].precio*carrito[i].cantidad;
+    }else{
+      total +=carrito[i].precio;
+    }
+  }
+  return total;
 }
 
-
+export function calcularPeso(carrito ){
+  var total=0;
+  for(var i=0 ;i<carrito.length; i++){
+    if(carrito[i].cantidad){
+      total+=carrito[i].peso*carrito[i].cantidad;
+    }else{
+      total +=carrito[i].peso;
+    }
+ }
+  return total;
+}
--- test.js
+++ test.js
@@ -1,6 +1,18 @@
-import { toRoman } from './index';
+import {calcularTotal,calcularPeso} from './index';
 import { equal } from "assert";
 
 describe(('Cálculo del total'), () => {
-  it('carrito vacío', () => { equal(calcularTotal([]), 0) })
+  it('carrito vacio',()=>{ equal(calcularTotal([]),0)})
+  it('comprar pan', () => { equal(calcularTotal([{producto:"pan",precio:100}]),100) })
+  it('comprar pan y leche', () => { equal(calcularTotal([{producto:"pan",precio:100},
+  {producto:"leche",precio:100}]),200)});
+   it('comprar huevos', () => { equal(calcularTotal([{producto:"huevo",precio:30, cantidad:12}
+  ]),360)});
+
+})
+describe(('cálculo del peso'),()=>{
+  it('carrito  vacio',()=>{equal(calcularPeso([]),0)});
+  it('peso pan',()=>{equal(calcularPeso([{producto:"pan",peso:80}]),80)})
+  it('peso pan',()=>{equal(calcularPeso([{producto:"pan",peso:80, cantidad:2}]),160)})
+ 
 })
